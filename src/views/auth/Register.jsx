5c07b120a67eba6e25f63066e65825fb74e468fd
Palashdada <palashnetworkgazipur@gmail.com>
2025-12-30 00:12:16 +0600

    first commit

---
+++ src/views/auth/Register.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className="min-w-screen min-h-screen bg-[#161d31] flex justify-center items-center">
+      <div className="w-[350px] text-[#d0d2d6] p-2">
+        <div className="bg-[#283046] p-4 rounded-md">
+          <h2>Welcome to Boonnee</h2>
+          <p className="text-sm mb-3">
+            Please Register to your account and start your business{" "}
+          </p>
+          <form>
+            <div className=" flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="name">Name</label>
+              <input
+                className="px-3 py-2  outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden"
+                type="text"
+                name="name"
+                placeholder="name"
+                id="name"
+                required
+              />
+            </div>
+            <div className=" flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="name">Email</label>
+              <input
+                className="px-3 py-2  outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden"
+                type="email"
+                name="Email"
+                placeholder="Email"
+                id="Email"
+                required
+              />
+            </div>
+            <div className=" flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="name">Password</label>
+              <input
+                className="px-3 py-2  outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden"
+                type="password"
+                name="Password"
+                placeholder="Password"
+                id="Password"
+                required
+              />
+            </div>
+            <div className=" flex items-center w-full gap-3 mb-3">
+              <input
+                className="w-4 h-4 text-blue-600 overflow-hidden bg-gray-100 rounded border-gray-300 focus:ring-blue-500"
+                type="checkbox"
+                name="Checkbox"
+                id="Checkbox"
+                required
+              />
+              <label htmlFor="Checkbox">
+                I agree to privacy policy & terms{" "}
+              </label>
+            </div>
+            <button className="bg-blue-500 w-full hover:shadow-blue-500/50 hover:shadow-lg text-white rounded-b-md px-7 py-2 mb-3 ">
+              Sing Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
