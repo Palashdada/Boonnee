cd46bce87da6c96454e4eb391c68f252c5c6688e
Palashdada <palashnetworkgazipur@gmail.com>
2025-12-30 04:19:08 +0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ const App = lazy(() => import("./App.jsx"));
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Boonnee">
     <Suspense>
       {" "}
       <App />
